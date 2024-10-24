41d826e7fcdc333fc309d832cc4abe6cde95a247
kshitij-codes <kshitijkvy@gmail.com>
2024-10-25 00:02:31 +0530

    feat: Initial Project setup including auth via clerk and database via prisma

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,25 @@
+// /api/clerk/webhook
+
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "../../../../server/db";
+
+export async function POST(request: NextRequest) {
+  const { data } = await request.json();
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.create({
+    data: {
+      emailAddress,
+      firstName,
+      lastName,
+      imageUrl,
+      id,
+    },
+  });
+
+  return new Response("Webhook received", { status: 200 });
+}
